739963b1e5450de3e697ab7b1cd2969ea8f05224
JoeyScottSchronce <166360909+JoeyScottSchronce@users.noreply.github.com>
2024-10-22 09:59:24 -0400

    updated images

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,56 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const avSlice = createSlice({
-  name: "av",
-  initialState: [
-    {
-        img: "https://pixabay.com/images/download/business-20031_640.jpg",
-        name: "Projectors",
-        cost: 200,
-        quantity: 0,
-    },
-    {
-        img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
-        name: "Speaker",
-        cost: 35,
-        quantity: 0,
-    },
-    {
-        img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
-        name: "Microphones",
-        cost: 45,
-        quantity: 0,
-    },
-    {
-        img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
-        name: "Whiteboards",
-        cost: 80,
-        quantity: 0,
-    },
+    name: "av",
+    initialState: [
+        {
+            img: "Conference-Event-Planner_lab_project/src/assets/images/projectors.jpg",
+            name: "Projectors",
+            cost: 200,
+            quantity: 0,
+        },
+        {
+            img: "Conference-Event-Planner_lab_project/src/assets/images/speakers.jpg",
+            name: "Speaker",
+            cost: 35,
+            quantity: 0,
+        },
+        {
+            img: "Conference-Event-Planner_lab_project/src/assets/images/microphones.jpg",
+            name: "Microphones",
+            cost: 45,
+            quantity: 0,
+        },
+        {
+            img: "Conference-Event-Planner_lab_project/src/assets/images/whiteboards.jpg",
+            name: "Whiteboards",
+            cost: 80,
+            quantity: 0,
+        },
 
-    {
-        img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
-        name: "Signage",
-        cost: 80,
-        quantity: 0,
-    },
+        {
+            img: "Conference-Event-Planner_lab_project/src/assets/images/signage.jpg",
+            name: "Signage",
+            cost: 80,
+            quantity: 0,
+        },
 
-  ],
-  
-  reducers: {
-    incrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item) {
-            item.quantity++;
-        }
+    ],
+
+    reducers: {
+        incrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item) {
+                item.quantity++;
+            }
+        },
+        decrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item && item.quantity > 0) {
+                item.quantity--;
+            }
+        },
     },
-    decrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item && item.quantity > 0) {
-            item.quantity--;
-        }
-    },
-  },
 });
 
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
